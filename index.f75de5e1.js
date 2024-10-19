64360823afb07c35fb478fcc12431bf6b554264b
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2024-10-19 16:24:54 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    notification.classList.add("notification", type);
+    const h2 = document.createElement("h2");
+    h2.classList.add("title");
+    h2.textContent = title;
+    const p = document.createElement("p");
+    p.textContent = description;
+    notification.appendChild(h2);
+    notification.appendChild(p);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        notification.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
